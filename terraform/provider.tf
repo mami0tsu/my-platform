02c3d970a21bc8e354058584d40af32234e34b22
mami0tsu <mami0tsu.jp@gmail.com>
2023-10-21 19:40:05 +0900

    add: terraform-cloud config

---
+++ terraform/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "1.6.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.22.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}

b468422728e27ecb44f6dec4dd405522fff24695
mami0tsu <mami0tsu.jp@gmail.com>
2023-10-22 20:51:13 +0900

    Init Terraform (#1)
    * add: github-actions config
    
    * add: terraform-cloud config
    
    * add: data.aws_iam_role.terraform_plan for test
    
    * add: terraform-cloud organization name
    
    * add: tmp resource for test
    
    * fix: workflow config
    
    * fix: working directory in workflow config
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix: terraform backend config
    
    * fix
    
    * fix
    
    * fix
    
    * fix for using Terraform Cloud
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix: actions config
    
    * fix
    
    * fix

---
+++ terraform/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.6.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.22.0"
+    }
+  }
+
+  cloud {
+    workspaces {
+      tags = ["stage:prd"]
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "TFC_AWS_PLAN_ROLE_ARN" {
+  type      = string
+  sensitive = true
+}
+
+variable "TFC_AWS_PROVIDER_AUTH" {
+  type = bool
+}

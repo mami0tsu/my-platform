fe5aad8f6ebb550c029e11277a23d00807d3fc98
mami0tsu <mami0tsu.jp@gmail.com>
2023-10-29 15:49:30 +0900

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,6 +8,11 @@ variable "TFC_AWS_PLAN_ROLE_ARN" {
   sensitive = true
 }
 
+variable "TFC_AWS_APPLY_ROLE_ARN" {
+  type      = string
+  sensitive = true
+}
+
 variable "TFC_AWS_PROVIDER_AUTH" {
   type = bool
 }
